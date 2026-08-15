b39453f8d4a4392f5eb92efe753f33dab7e067ac
xhdskyprime <xhdprime@gmail.com>
2026-08-15 18:16:47 +0700

    Fix broken Bank BJB logo by using correct Wikipedia ID PNG URL

--- js/invitation-final.js
+++ js/invitation-final.js
@@ -14,7 +14,7 @@ const INDONESIA_BANKS = [
   { code: "OVO", name: "OVO", logo: "https://upload.wikimedia.org/wikipedia/commons/eb/e8/OVO_Logo.svg" },
   { code: "DANA", name: "DANA", logo: "https://upload.wikimedia.org/wikipedia/commons/7/72/Logo_DANA.svg" },
   { code: "SHOPEEPAY", name: "ShopeePay", logo: "https://upload.wikimedia.org/wikipedia/commons/f/fe/ShopeePay_logo.svg" },
-  { code: "BJB", name: "Bank BJB", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Logo_bank_bjb.svg" },
+  { code: "BJB", name: "Bank BJB", logo: "https://upload.wikimedia.org/wikipedia/id/thumb/4/41/Bank_BJB_logo.svg/500px-Bank_BJB_logo.svg.png" },
   { code: "LAINNYA", name: "Bank / E-Wallet Lain", logo: "" }
 ];
 
